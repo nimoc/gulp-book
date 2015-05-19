815cc622a5cafa3976466579c232897422cc399c
trainGitHub <xzarpud@gmail.com>
2015-05-19 17:27:20 +0800

    删除多余括号

--- demo/chapter4/gulpfile.js
+++ demo/chapter4/gulpfile.js
@@ -20,7 +20,7 @@ gulp.task('images', function () {
 // 在命令行使用 gulp auto 启动此任务
 gulp.task('auto', function () {
     // 监听文件修改，当文件被修改则执行 images 任务
-    gulp.watch('images/*.*)', ['images'])
+    gulp.watch('images/*.*', ['images'])
 });
 
 // 使用 gulp.task('default') 定义默认任务
